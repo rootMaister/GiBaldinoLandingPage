e99b6c21444691bdf0626fa9cb5f4f484cb6f272
Vitor Costa <falkinerfx@gmail.com>
2024-01-30 16:22:24 -0300

    Colors added as variables to be able to apply features between Gi Baldino and Multi.

---
+++ knowMulti.js
@@ -0,0 +1,9 @@
+const html = document.querySelector('html');
+const button = document.querySelector('#knowMulti');
+
+if(button){
+  button.addEventListener("click", function(){
+    html.classList.toggle('multiMode');
+    console.log("O botão existe")
+  })
+}
